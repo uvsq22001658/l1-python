6203114e5f8018759aec42b5cda7a6e476d3b888
Julie VALBERT <julie.valbert@ens.uvsq.fr>
2020-11-04 18:41:16 +0100

    td3

---
+++ exercises/TD03_fonctions/td3.py
@@ -0,0 +1,22 @@
+# temps[0] : jours, temps[1]: minutes, temps[2]: minutes, temps[3]: secondes
+
+def tempsEnSeconde(temps):
+    """ Renvoie la valeur en seconde de temps donné
+    comme jour, heure, minute, seconde."""
+    return temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
+
+
+temps = (3, 23, 1, 34)
+print(type(temps))
+print(tempsEnSeconde(temps))
+
+
+def secondeEnTemps(seconde):
+    """Renvoie le temps (jour, heure, minute, seconde) qui correspond
+    au nombre de seconde passé en argument"""
+    return temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
+
+
+temps = secondeEnTemps(100000)
+print(temps[0], "jours", temps[1], "heures", temps[2], "minutes",
+                temps[3], "secondes")
