fda1a1ff846ed066d2e5adb36f92b4bd92232339
Julie VALBERT <julie.valbert@ens.uvsq.fr>
2020-11-16 13:40:04 +0100

    exos supp

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -1,8 +1,8 @@
 # temps[0] : jours, temps[1]: minutes, temps[2]: minutes, temps[3]: secondes
 
 def tempsEnSeconde(temps):
-    """ Renvoie la valeur en seconde de temps donné
-    comme jour, heure, minute, seconde."""
+    """ Renvoie la valeur en seconde de temps donné"""
+    """comme jour, heure, minute, seconde."""
     return temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
 
 
@@ -14,7 +14,13 @@ print(tempsEnSeconde(temps))
 def secondeEnTemps(seconde):
     """Renvoie le temps (jour, heure, minute, seconde) qui correspond
     au nombre de seconde passé en argument"""
-    return temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
+    jour = seconde // 86400
+    j = seconde % 86400
+    heure = j // 3600
+    h = j % 3600
+    minute = h // 60
+    seconde = h % 60
+    return(jour, heure, minute, seconde)
 
 
 temps = secondeEnTemps(100000)
